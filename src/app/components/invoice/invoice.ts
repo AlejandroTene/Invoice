c183cc2a0b0eaf895a3519bf6a1dbeac56f61634
AlejandroTene <carlostene2006@gmail.com>
2025-08-14 09:09:30 -0500

    reto10

--- src/app/components/invoice/invoice.ts
+++ src/app/components/invoice/invoice.ts
@@ -5,20 +5,27 @@ import { CompanyView } from "../company-view/company-view";
 import { CustomerView } from "../customer-view/customer-view";
 import { InvoiceDetail } from "../invoice-detail/invoice-detail";
 
-
 @Component({
   selector: 'app-invoice',
   imports: [CompanyView, CustomerView, InvoiceDetail],
   templateUrl: './invoice.html',
+  styleUrl: './invoice.css',
 })
+
 export class InvoiceComponent implements OnInit {
-
-  invoice!:Invoice;
-  constructor(private service:InvoiceService){}
-
+  invoice!: Invoice;
+  constructor(private service: InvoiceService) { }
   ngOnInit(): void {
-    this.invoice=this.service.getInvoice();
+    this.service.getInvoice().subscribe(
+      (data: Invoice) => {
+        this.invoice = data;
+        console.log('Datos recibidos del backend:', data);
+      },
+      (error) => {
+        console.error('Error al obtener datos del backend:', error); 
+        console.error('Asegúrate de que el backend esté en puerto 3000');
+      }
+    );
   }
-
-
 }
+
